44714368b81eb975c35f9abaafe45add46649952
rafaelAugust0 <rafael.augustoo1612@gmail.com>
2022-12-02 19:05:57 -0300

    introduced new style for the contact section page (CONTATOS)

--- src/pages/Contatos.js
+++ src/pages/Contatos.js
@@ -26,51 +26,58 @@ const Contato = () => {
                     </Row>
                 </Container>
             </div>
-                <div className="contact__infos">
-                    <Container className="py-5">
-                        <Row className="d-flex flex-lg-row flex-column">
-                            <Col>
-                                <Container>
-                                    <div className="numbers my-3 py-5 text-center">
-                                        <h3>Liga pra gente</h3>
-                                        <FontAwesomeIcon icon={faPhone} color="#ffc500" className="mx-2"/>
-                                        <span>+55 47 0000-0000</span>
-                                        <FontAwesomeIcon icon={faWhatsapp} color="#ffc500" className="mx-2"/>
-                                        <span>+55 47 0000-0000</span>
-                                    </div>
-                                    <div className="address my-5 text-center">
-                                    <h3>Venha nos fazer uma visita</h3>
-                                        <FontAwesomeIcon icon={faMap} color="#ffc500" className="mx-2"/>
-                                        <span>Rua Miguel Daux, 129 - Coqueiros Florianópolis / SC</span>
-                                    </div>
-                                </Container>
-                            </Col>
-                            <Col>
-                                <Container>
-                                    <div className="form-contact">
-                                        <h3 className="text-center">Deixe sua mensagem</h3>
-                                        <Form autoComplete="off">
-                                            <Form.Group className="mb-3" controlId="formBasicName">
-                                                <Form.Label>Seu nome</Form.Label>
-                                                <Form.Control type="text"/>
-                                            </Form.Group>
-                                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                                <Form.Label>Seu email</Form.Label>
-                                                <Form.Control type="email"/>
-                                            </Form.Group>
-                                            <Form.Group className="mb-3" controlId="formBasicText">
-                                                <Form.Label>Sua mensagem</Form.Label>
-                                                <textarea className="form-control" rows="5"/>
-                                            </Form.Group>
-                                            <Button variant="light" className="my-3 px-5" size="lg">
-                                                Enviar
-                                            </Button>
-                                        </Form>
-                                    </div>
-                                </Container>
-                            </Col>
-                        </Row>
-                    </Container>
+                <div className="contact__container d-flex flex-lg-row flex-column">
+                    <div className="contact__infos w-lg-50 w-100">
+                        <Container className="py-5">
+                            <Row className="d-flex flex-lg-row flex-column">
+                                <Col>
+                                    <Container>
+                                        <div className="my-3 py-5 text-center">
+                                            <h3>Liga pra gente</h3>
+                                            <FontAwesomeIcon icon={faPhone} color="#333" className="mx-2"/>
+                                            <span>+55 47 0000-0000</span>
+                                            <FontAwesomeIcon icon={faWhatsapp} color="#333" className="mx-2"/>
+                                            <span>+55 47 0000-0000</span>
+                                        </div>
+                                        <div className="address my-5 text-center">
+                                        <h3>Venha nos fazer uma visita</h3>
+                                            <FontAwesomeIcon icon={faMap} color="#333" className="mx-2"/>
+                                            <span>Rua Miguel Daux, 129 - Coqueiros Florianópolis / SC</span>
+                                        </div>
+                                    </Container>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+                    <div className="contact__form w-lg-50 w-100 py-5">
+                        <Col>
+                            <Container>
+                                <div className="form-contact">
+                                    <h3 className="text-center py-3">Deixe sua mensagem</h3>
+                                    <Form autoComplete="off">
+                                        <Form.Group className="mb-3" controlId="formBasicName">
+                                            <Form.Label>Seu nome</Form.Label>
+                                            <Form.Control type="text"/>
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                                            <Form.Label>Seu email</Form.Label>
+                                            <Form.Control type="email"/>
+                                        </Form.Group>
+                                        
+                                        <Form.Group className="mb-3" controlId="formBasicText">
+                                            <Form.Label>Sua mensagem</Form.Label>
+                                            <textarea className="form-control" rows="5"/>
+                                        </Form.Group>
+
+                                        <Button variant="warning" className="my-3 px-5" size="lg">
+                                            Enviar
+                                        </Button>
+                                    </Form>
+                                </div>
+                            </Container>
+                        </Col>
+                    </div>
                 </div>
         </section>
     )
